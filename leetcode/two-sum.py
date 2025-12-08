418931bedfd97ce433a3e88adcf8382644f78d1c
Dhruv_Prajapati <dhruvrama6@gmail.com>
2025-12-08 22:37:06 +0530

    Update two-sum.py

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,1 +1,9 @@
+# LeetCode 1: Two Sum
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i, num in enumerate(nums):
+            if target - num in hashmap:
+                return [hashmap[target - num], i]
+            hashmap[num] = i
 
